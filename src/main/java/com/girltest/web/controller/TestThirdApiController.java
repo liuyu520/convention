778654a0d1479a6322177a17483f31d130689112
huangweii <huangweii@chanjet.com>
2016-04-09 18:19:32 +0800

    增加捕获异常:java.net.UnknownHostException

--- src/main/java/com/girltest/web/controller/TestThirdApiController.java
+++ src/main/java/com/girltest/web/controller/TestThirdApiController.java
@@ -1,11 +1,12 @@
 package com.girltest.web.controller;
 
+import com.common.util.SystemHWUtil;
 import com.io.hw.json.HWJacksonUtils;
 import com.string.widget.util.ValueWidget;
+import com.string.widget.util.XSSUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.util.HtmlUtils;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -20,9 +21,11 @@ import java.util.Map;
 @Controller
 @RequestMapping("/testapi")
 public class TestThirdApiController {
-    @RequestMapping("/testapi")
+
+    @RequestMapping(value = "/testapi"/*, produces = SystemHWUtil.RESPONSE_CONTENTTYPE_JSON_UTF*/)
     @ResponseBody
     public String test(String apiPath, String requestMethod) throws IOException {
+        apiPath = XSSUtil.deleteXSS(apiPath);
         URL url = new URL(apiPath);
         URLConnection urlConnection = url.openConnection();
         HttpURLConnection httpUrlConnection = (HttpURLConnection) urlConnection;
@@ -31,13 +34,18 @@ public class TestThirdApiController {
         if (!ValueWidget.isNullOrEmpty(requestMethod)) {
             httpUrlConnection.setRequestMethod(requestMethod);
         }
-        httpUrlConnection.connect();
-        int responseStatusCode = httpUrlConnection.getResponseCode();
+        int responseStatusCode = SystemHWUtil.NEGATIVE_ONE;
+        try {
+            httpUrlConnection.connect();
+            responseStatusCode = httpUrlConnection.getResponseCode();
+        } catch (java.net.UnknownHostException ex) {
+            ex.printStackTrace();
+        }
+
         httpUrlConnection.disconnect();
         System.out.println("responseStatusCode:" + responseStatusCode);
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("responseCode", responseStatusCode);
-        apiPath = HtmlUtils.htmlEscape(apiPath);
         map.put("apiPath", apiPath);
         return HWJacksonUtils.getJsonP(map);
     }
