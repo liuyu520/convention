0b6ca5e6f40979558e2efbb9efdfb3efb94b45d9
huangweii <huangweii@chanjet.com>
2016-09-27 11:34:26 +0800

    增加日志实体类和dao

---
+++ src/main/java/com/girltest/dao/DiaryDao.java
@@ -0,0 +1,68 @@
+package com.girltest.dao;
+
+import com.common.dao.generic.GenericDao;
+import com.girltest.entity.Diary;
+import com.time.util.TimeHWUtil;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Component;
+
+import java.util.Calendar;
+import java.util.Date;
+
+@Component("diaryDao")
+public class DiaryDao extends GenericDao<Diary> {
+
+
+    /***
+     * 获取当前时段的日记
+     * @return
+     */
+    public Diary getCurrent(){
+        Calendar morning0Calendar=Calendar.getInstance();
+        morning0Calendar.set(Calendar.HOUR_OF_DAY,0);
+        morning0Calendar.set(Calendar.MINUTE,0);
+        morning0Calendar.set(Calendar.SECOND,0);
+        Date morning0=morning0Calendar.getTime();
+        System.out.println(TimeHWUtil.formatDateTime(morning0));
+
+        morning0Calendar.set(Calendar.HOUR_OF_DAY,12);
+        Date morning12=morning0Calendar.getTime();
+
+
+        morning0Calendar.set(Calendar.HOUR_OF_DAY,23);
+        morning0Calendar.set(Calendar.MINUTE,59);
+        morning0Calendar.set(Calendar.SECOND,59);
+
+
+        Date morning24=morning0Calendar.getTime();
+
+        Date now=new Date();
+        String startDate=null;
+        String endDate=null;
+        if(now.compareTo(morning12)>0){
+            System.out.println("下午");
+            startDate=TimeHWUtil.formatDateTime(morning12);
+            endDate=TimeHWUtil.formatDateTime(morning24);
+        }else{
+            System.out.println("上午");
+            startDate=TimeHWUtil.formatDateTime(morning0);
+            endDate=TimeHWUtil.formatDateTime(morning12);
+        }
+        Criteria criteria= super.getCurrentSession().createCriteria(Diary.class).add(Restrictions.between("createTime",startDate,endDate));
+        Diary diary= (Diary)criteria.uniqueResult();
+        return diary;
+    }
+    public Diary create(){
+        Diary diary=getCurrent();
+        if(null==diary){
+            diary=new Diary();
+            Date now=new Date();
+            String nowStr=TimeHWUtil.formatDateTime(now);
+            diary.setCreateTime(nowStr);
+            diary.setUpdateTime(nowStr);
+            save(diary);
+        }
+        return diary;
+    }
+}
---
+++ src/main/java/com/girltest/entity/Diary.java
@@ -0,0 +1,95 @@
+package com.girltest.entity;
+
+
+import javax.persistence.*;
+
+/**
+ * Created by 黄威 on 9/18/16.<br >
+ */
+@Entity
+@Table(name = "t_diary")
+public class Diary {
+    private int id;
+    /***
+     * 更新时间
+     */
+    protected String updateTime;
+    /***
+     * 创建时间
+     */
+    protected String createTime;
+    private String content;
+    /***
+     * 创建者
+     */
+    private User user;
+    /***
+     * 1:有效;2:被删除
+     */
+    private int status;
+    /***
+     * 热度 <br>
+     * 值越大,表示越受关注
+     */
+    protected Integer stars;
+
+    @Id
+    @GeneratedValue
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Integer getStars() {
+        return stars;
+    }
+
+    public void setStars(Integer stars) {
+        this.stars = stars;
+    }
+}
