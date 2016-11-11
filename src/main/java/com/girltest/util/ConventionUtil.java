361f035bedd7f4b929411951221004d5aa865b51
huangweii <huangweii@chanjet.com>
2016-11-11 09:17:49 +0800

    解决搜索答案结果列表(http://blog.yhskyc.com/convention2/convention/list),修改之后无法保存的问题

--- src/main/java/com/girltest/util/ConventionUtil.java
+++ src/main/java/com/girltest/util/ConventionUtil.java
@@ -18,7 +18,10 @@ public class ConventionUtil {
                 || "性观念".equals(queryKeyword)
                 || "做爱".equals(queryKeyword)
                 || "性爱".equals(queryKeyword)
-                || "rr大姨妈".equals(queryKeyword)) {
+                || "rr大姨妈".equals(queryKeyword)
+                || "rr生日".equals(queryKeyword)
+                || "rr爱好".equals(queryKeyword)
+                || "rr习惯".equals(queryKeyword)) {
             return true;
         }
         return false;
